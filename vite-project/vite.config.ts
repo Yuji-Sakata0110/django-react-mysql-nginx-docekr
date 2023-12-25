6778581f5e9efe88b7796364c273bbc892f61bac
yujis <yuji.sakata0110@gmail.com>
2023-12-25 21:37:22 +0900

    hot reload test

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -6,9 +6,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    hmr: {
-      protocol: "ws", // Use WebSocket for Hot Module Replacement
-    },
   },
   plugins: [react()],
 });
